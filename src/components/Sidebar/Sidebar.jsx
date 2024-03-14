7026cf54d897bd7df26ea176f66617215257e8c6
Gnaneswari <gnlolugu@iu.edu>
2024-03-13 20:55:52 -0400

    search users

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,33 +4,11 @@ import {CreatePostLogo, InstagramLogo, InstagramMobileLogo, NotificationsLogo, S
 import {AiFillHome} from 'react-icons/ai';
 import { BiLogOut } from 'react-icons/bi'
 import useLogout from "../../hooks/useLogout";
+import SidebarItems from "./SidebarItems";
 
 const Sidebar = () => {
   const {handleLogout,isLoggingout, } = useLogout();
-  const sidebarItems = [
-    {
-      icon: <AiFillHome size={25} />,
-      text:"Home",
-      link:"/",
-    },
-    {
-      icon: <SearchLogo />,
-      text:"Search",
-    },
-    {
-      icon: <NotificationsLogo />,
-      text: "Notifications",
-    },
-    {
-      icon: <CreatePostLogo />,
-      text: "Create",
-    },
-    {
-      icon: <Avatar size={"sm"} name="Barak Orkmez" src="/profilepic.png" />,
-      text: "Profile",
-      link:"/asaprogrammer",
-    },
-  ];
+  
   return (
     
     <Box height={"100vh"} borderRight={'1px solid'} borderColor={"whiteAlpha.300"} py={8} position={"sticky"} to={0} left={0} px={{base:2, md:4}}>
@@ -46,32 +24,7 @@ const Sidebar = () => {
                 <InstagramMobileLogo />
             </Link>
             <Flex direction={"column"} gap={5} cursor={"pointer"}>
-              {sidebarItems.map((item,index) =>(
-                <Tooltip
-                  key={index}
-                  hasArrow
-                  label={item.text}
-                  placement="right"
-                  ml={1}
-                  openDelay={500}
-                  display={{base:'block',md:'none'}}>
-                  <Link
-                  display={"flex"}
-                  to={item.link || null}
-                  as={RouterLink}
-                  alignItems={'center'}
-                  gap={4}
-                  _hover ={{bg:"whiteAlpha.400"}}
-                  borderRadius={6}
-                  p={2}
-                  w={{base:10, md:"full"}}
-                  justifyContent={{base:"center",md:"flex-start"}}
-                  >
-                {item.icon}
-                    <Box display={{base:"none", md:"block"}}>{item.text}</Box>
-                  </Link>
-                </Tooltip>
-              ))}
+              <SidebarItems />
             </Flex>
             <Tooltip
                   hasArrow
--- src/components/SuggestedUsers/SuggestedUser.jsx
+++ src/components/SuggestedUsers/SuggestedUser.jsx
@@ -1,27 +1,41 @@
 import { Avatar, Flex, Text, VStack, Button } from "@chakra-ui/react"
-import { useState } from "react"
+import useFollowUser from "../../hooks/useFollowUser"
+import useAuthStore from "../../store/authStore"
 
-const SuggestedUser = ({avatar, name, followers}) => {
-    const [isFollowed,setIsFollowed] = useState(false)
+const SuggestedUser = ({user,setUser}) => {
+    const {isUpdating,isFollowing,handleFollowUser} = useFollowUser(user.uid)
+    const authUser = useAuthStore((state) =>  state.user)
+
+    const onFollowUser = async () =>{
+        await handleFollowUser();
+        setUser({
+            ...user,
+            followers: isFollowing ? user.followers.filter((follower)=> follower.uid !==authUser.uid)
+                    : [...user.followers, authUser],
+        })
+    }
   return (
     <Flex justifyContent={'space-between'} w={'full'} py={2}>
         <Flex gap={4}>
-            <Avatar src={avatar}/>
+            <Avatar src={user.profilePicURL}/>
             <VStack alignItems={'start'}>
-                <Text fontSize={14} alignSelf={'center'} fontWeight={'bold'}>{name}</Text>
-                <Text fontWeight={'bold'} color={'gray.500'} fontSize={12}>{followers} followers</Text>
+                <Text fontSize={14} alignSelf={'center'} fontWeight={'bold'}>{user.fullname}</Text>
+                <Text fontWeight={'bold'} color={'gray.500'} fontSize={12}>{user.followers.length} followers</Text>
             </VStack>
         </Flex>
-        <Button alignSelf={'center'}
-        color={'blue.500'} 
-        cursor={'pointer'} 
-        _hover={{color:'white'}} 
-        fontWeight={'bold'} 
-        fontSize={14}
-        onClick={() => {setIsFollowed(!isFollowed)}}
-        bg={'transparent'}>
-            {isFollowed ? "UnFollow" : "Follow"}
-        </Button>
+        {authUser.uid !== user.uid && (
+            <Button alignSelf={'center'}
+                color={'blue.500'} 
+                cursor={'pointer'} 
+                _hover={{color:'white'}} 
+                fontWeight={'bold'} 
+                fontSize={14}
+                onClick={onFollowUser}
+                bg={'transparent'}
+                isLoading={isUpdating}>
+                    {isFollowing ? "UnFollow" : "Follow"}
+            </Button>
+        )}
     </Flex>
   )
 }
